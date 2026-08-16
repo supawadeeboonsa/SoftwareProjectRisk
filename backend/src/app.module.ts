d1a663dac2fcfd4dcfe51b91e12fe755b7d9d831
Supawadee Boonsa <baipo2511@gmail.com>
2026-08-16 14:28:16 +0700

    hello world

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProjectModule } from './project/project.module';
 
 @Module({
-  imports: [PrismaModule, ProjectModule],
+  imports: [
+    PrismaModule,
+    ProjectModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
